3309d9be715d8aae5ef61f37520e40ea7425f4d4
Kasper <147148480+0xh34z@users.noreply.github.com>
2025-10-09 12:19:44 +0200

    Update cleanup.bat

--- cleanup.bat
+++ cleanup.bat
@@ -1,8 +1,7 @@
 @echo off
-set "TARGET_DIR=D:\UFED"
+set "TARGET_DIR=D:\TARGET DIRECTORY"
 if not exist "%TARGET_DIR%" (
     echo Directory not found: %TARGET_DIR%
     exit /b 1
 )
 forfiles /P "%TARGET_DIR%" /D -30 /C "cmd /c if @isdir==TRUE (rmdir /s /q @path) else (del @path)"
-echo Files en directories ouder dan 30 dagen in %TARGET_DIR% zijn verwijderd.
